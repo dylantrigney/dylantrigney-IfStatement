516cb418375444d1e1541289db66420984f2f07a
dylantrigney <dylan.rigney@revature.net>
2023-02-17 19:35:13 +0000

    if complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -17,7 +17,7 @@ public class Lab {
      * @param bool a true/false value that determines if x or y is to be returned.
      * @param x first number to be compared.
      * @param y second number to be compared.
-     * @return x if bool is true. if bool is false, return y.
+     * @return x if bool is true. if bool is false, return 
      */
     public int ifExample(boolean bool, int x, int y){
         if (bool == true) {
